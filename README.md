0747dc11186313940a11c77c10561515aa2db582
Karel Uhlík <karel.uhlik@atlas.cz>
2016-12-02 15:52:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# DateTimeCalculation
+ smart date time calculation missing in PHP
+ From provided date you can get information of:
+  - quarter
+  - first day of quarter
+  - last day of quarter
+  - first month of quarter
+  - last month of quarter
+  - days in quarter
+  - days passed in quarter
---
+++ src/DateTimeCalculation.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace ApliTax;
+
+class DateTimeCalculation 
+{
+	/** @var \Datetime */
+	private $date;
+	
+	public function __construct(\DateTime $date = NULL) {
+		if (!$date) {
+			$date = new \DateTime();
+		}
+		$this->date = $date;
+	}
+
+	/**
+	* Calculates first day of quarter
+	* 
+	* @param DateTime $date
+	* @return DateTime
+	*/
+	public function firstDayOfQuarter() {
+		$m = $this->firstMonthOfQuarter();
+		return new \DateTime($this->date->format("Y") . "-" . $m . "-01");
+	}
+
+	/**
+	* Calculates last day of quarter
+	* 
+	* @param DateTime $date
+	* @return DateTime
+	*/
+	public function lastDayOfQuarter() {
+		$m = $this->lastMonthOfQuarter();
+		$return = new \DateTime($this->date->format("Y") . "-" . $m . "-01 23:59:59");
+		return $return->modify('last day of');
+	}
+	
+	/**
+	* Calculates first month of quarter
+	* 
+	* @param DateTime $date
+	* @return int
+	*/
+	public function firstMonthOfQuarter() {
+		$q = $this->quarter($this->date);
+		return 1 + ($q - 1) * 3;
+	}
+
+	/**
+	* Calculates last month of quarter
+	* 
+	* @param DateTime $date
+	* @return int
+	*/
+	public function lastMonthOfQuarter() {
+		$q = $this->quarter($this->date);
+		return $q * 3;
+	}
+	
+	/**
+	* Calculates quarter from date
+	* 
+	* @param DateTime $date
+	* @return int
+	*/
+	public function quarter() {
+		return floor(($this->date->format("n") - 1) / 3) + 1;
+	}
+	
+	/**
+	* Calculates total days in quarter
+	* 
+	* @param DateTime $date
+	* @return int
+	*/
+	public function daysInQuarter() {
+		$d1 = $this->firstDayOfQuarter();
+		$d2 = $this->lastDayOfQuarter();
+		$period = $d1->diff($d2);
+		return $period->format("%a") + 1;
+	}
+
+	/**
+	* Calculates days passed in quarter
+	* 
+	* @param DateTime $date
+	* @return int
+	*/
+	public function daysPassedInQuarter() {
+		$d1 = $this->firstDayOfQuarter();
+		$period = $d1->diff($this->date);
+		return $period->format("%a");
+	}
+	
+	public function info() {
+		setlocale(LC_ALL, "");
+		echo "<pre>";
+		echo "Date                   : " . $this->date->format("d.m.Y H:i:s") . "\n";
+		echo "Qarter                 : " . $this->quarter() . ".\n";
+		echo "First day of quarter   : " . $this->firstDayOfQuarter()->format("d.m.Y") . "\n";
+		echo "Last  day of quarter   : " . $this->lastDayOfQuarter()->format("d.m.Y") . "\n";
+		echo "First month of quarter : " . $this->firstDayOfQuarter()->format("F") . "\n";
+		echo "First month of quarter : " . $this->firstMonthOfQuarter() . "\n";
+		echo "Last  month of quarter : " . $this->lastDayOfQuarter()->format("F") . "\n";
+		echo "Last  month of quarter : " . $this->lastMonthOfQuarter() . "\n";
+		echo "Days in quarter        : " . $this->daysInQuarter() . "\n";
+		echo "Days passed in quarter : " . $this->daysPassedInQuarter() . "\n";
+		echo "</pre>";
+	}
+	
+}
